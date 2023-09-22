afc48d09cc507eb1ba995720c5ae5adff2d22d54
Samba <90577515+Samba73@users.noreply.github.com>
2023-09-23 00:33:25 +0530

    11 simple s3 bucket (#12)
    * #11 Add AWS provider and created S3 bucket
    
    * #11 Updated README.md

--- main.tf
+++ main.tf
@@ -4,8 +4,18 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
+# Configure the AWS provider
+provider "aws" {
+  # Configuration options
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 
 provider "random" {
   # Configuration options
@@ -13,6 +23,17 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  upper            = false 
+}
+
+# Define s3 in terraform
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+
+# s3 naming rules
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+
+resource "aws_s3_bucket" "example" {
+  bucket = "samba07-tf-${random_string.bucket_name.result}"
 }
 
 output "random_bucket_name" {
