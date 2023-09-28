8fbbf04ea482d21a85851381e99ea20186c4c227
Samba <90577515+Samba73@users.noreply.github.com>
2023-09-28 15:39:09 +0530

    #23 Fix state file issue with import and fix configuration drift (#24)

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
 
-resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
-  upper            = false 
-}
-
 # Define s3 in terraform
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
 # s3 naming rules
 # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
 
-resource "aws_s3_bucket" "example" {
-  bucket = "samba07-tf-${random_string.bucket_name.result}"
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.s3_bucket_name
 
   tags = {
     "Created_By" = var.user_uuid
