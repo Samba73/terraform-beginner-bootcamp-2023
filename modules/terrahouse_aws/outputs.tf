fe76e44cbc37e012e94f91ddd16d09dc047c0a5d
Samba73 <sambasivam.k@gmail.com>
2023-09-29 18:10:10 +0000

    #34 Invalidated CF distribution when content version is changed

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,7 +1,11 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.website_bucket
+  value = aws_s3_bucket.website_bucket.bucket
 }
 
 output "s3_website_url" {
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "s3_bucket_name" {
 output "s3_website_url" {
   value = module.terrahouse_aws.s3_website_url
 }
+
+output "cloudfront_url" {
+  value = module.terrahouse_aws.cloudfront_url
+}
