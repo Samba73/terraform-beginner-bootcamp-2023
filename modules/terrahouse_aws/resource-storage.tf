1a6615b9c79692c3af72d77a868ebe6f44026f47
Samba <90577515+Samba73@users.noreply.github.com>
2023-09-29 14:49:12 +0530

    Update resource-storage.tf

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -55,7 +55,7 @@ resource "aws_s3_bucket_policy" "cf_bucket_policy" {
         "Resource": "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
         "Condition": {
             "StringEquals": {
-                # "AWS:SourceArn": "arn:aws:cloudfront::<AWS account ID>:distribution/<CloudFront distribution ID>"
+                # "AWS:SourceArn": "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
                 "AWS:SourceArn": aws_cloudfront_distribution.s3_distribution.arn
             }
         }
