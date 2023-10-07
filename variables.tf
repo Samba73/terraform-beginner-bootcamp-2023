6e466bedf827b7376378342217260cc74bc1a0a6
Samba73 <sambasivam.k@gmail.com>
2023-10-07 10:39:29 +0000

    #50 Made minor changes to variable declaration. nothing major from completing in previous commit.

--- variables.tf
+++ variables.tf
@@ -1,24 +1,13 @@
-# variable "terratowns_access_token" {
-#   type        = string 
-# }
-# variable "terratowns_endpoint" {
-#   type        = string 
-# }
-# variable "terratowns_town" {
-#   type        = string 
-# }
-# variable "user_uuid" {
-#   description = "The user UUID who created this bucket"
-#   type        = string
-# }
 variable "terratowns_schema" {
   type        = object({
-    access_token = string
+    //access_token = string
     endpoint     = string
     town         = string
-    user_uuid    = string 
+    //user_uuid    = string 
   })
 }
+variable "user_uuid" {}
+variable "access_token" {}
 
 variable "s3_bucket_name" {
   description = "The bucket name for the website"
@@ -27,20 +16,7 @@ variable "s3_bucket_name" {
     crepe     = string
   })
 }
-# variable "index_html_path" {
-#   description = "Path to index.html file for static website"
-# }
-# variable "error_html_path" {
-#   description = "Path to error.html file for static website"
-# }
-# variable "content_version" {
-#   description = "A positive integer starting from 1"
-#   type        = number
-# }
-# variable "assets_path" {
-#   description = "Path to a file"
-#   type        = string
-# }
+
 variable "pac-man" {
   type        = object({
     public_path = string
